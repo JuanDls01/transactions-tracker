4b335c7229a5edec21430ce810e9eda05e7fd569
unknown <juanignaciodelossantos01@gmail.com>
2025-01-11 17:32:42 -0300

    feat(ui): added error messages to input and select components

--- src/ui/elements/Input.tsx
+++ src/ui/elements/Input.tsx
@@ -1,18 +1,27 @@
 import { clsx } from "clsx";
 
-type InputPropsType = React.InputHTMLAttributes<HTMLInputElement>;
+type InputPropsType = {
+  errorMessage?: string;
+} & React.InputHTMLAttributes<HTMLInputElement>;
 
-const Input = (props: InputPropsType) => {
+const Input = ({ errorMessage, ...props }: InputPropsType) => {
   return (
-    <input
-      className={clsx(
-        "w-full min-w-0 py-1.5 px-3",
-        "text-gray-900 sm:text-sm/6 text-base",
-        "rounded-md outline outline-1 -outline-offset-1 outline-gray-300",
-        "focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600",
+    <>
+      <input
+        className={clsx(
+          "w-full min-w-0 py-1.5 px-3",
+          "text-gray-900 sm:text-sm/6 text-base",
+          "rounded-md outline outline-1 -outline-offset-1 outline-gray-300",
+          "focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600",
+        )}
+        {...props}
+      />
+      {errorMessage && (
+        <p role="alert" className="text-red-500 text-xs italic">
+          {errorMessage}
+        </p>
       )}
-      {...props}
-    />
+    </>
   );
 };
 
--- src/ui/elements/Select.tsx
+++ src/ui/elements/Select.tsx
@@ -1,31 +1,37 @@
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
 import { clsx } from "clsx";
 
-type SelectPropsType = React.SelectHTMLAttributes<HTMLSelectElement>;
+type SelectPropsType = {
+  errorMessage?: string;
+} & React.SelectHTMLAttributes<HTMLSelectElement>;
 
-const Select = ({ children, ...props }: SelectPropsType) => {
+const Select = ({ errorMessage, children, ...props }: SelectPropsType) => {
   return (
-    <div className={clsx("grid grid-cols-1", "w-full min-w-0")}>
-      <select
-        id="category"
-        name="category"
-        aria-label="Categoría"
-        {...props}
-        className={clsx(
-          "col-start-1 row-start-1 w-full",
-          "py-1.5 pl-3 pr-7",
-          "text-base text-gray-500 placeholder:text-gray-400 sm:text-sm/6",
-          "appearance-none rounded-md outline outline-1 -outline-offset-1 outline-gray-300",
-          "focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600",
-        )}
-      >
-        {children}
-      </select>
-      <ChevronDownIcon
-        aria-hidden="true"
-        className="col-start-1 row-start-1 pointer-events-none mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-      />
-    </div>
+    <>
+      <div className={clsx("grid grid-cols-1", "w-full min-w-0")}>
+        <select
+          {...props}
+          className={clsx(
+            "col-start-1 row-start-1 w-full",
+            "py-1.5 pl-3 pr-7",
+            "text-base text-gray-500 placeholder:text-gray-400 sm:text-sm/6",
+            "appearance-none rounded-md outline outline-1 -outline-offset-1 outline-gray-300",
+            "focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600",
+          )}
+        >
+          {children}
+        </select>
+        <ChevronDownIcon
+          aria-hidden="true"
+          className="col-start-1 row-start-1 pointer-events-none mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+        />
+      </div>
+      {errorMessage && (
+        <p role="alert" className="text-red-500 text-xs italic">
+          {errorMessage}
+        </p>
+      )}
+    </>
   );
 };
 
