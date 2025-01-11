bf47e248f99b35584b7446ac8a9bd05d3ee57d86
unknown <juanignaciodelossantos01@gmail.com>
2025-01-11 17:34:12 -0300

    refactor(transaction-form): created consts, schema and types files

---
+++ src/types/transactions.ts
@@ -0,0 +1,23 @@
+export enum TransactionCategory {
+  Leisure = "LEISURE",
+  Health = "HEALTH",
+  Home = "HOME",
+  Subscriptions = "SUBSCRIPTIONS",
+  Car = "CAR",
+  Food = "FOOD",
+  SavingsInvestments = "SAVINGS/INVESTMENTS",
+  Education = "EDUCATION",
+  Travel = "TRAVEL",
+  Work = "WORK",
+  Miscellaneous = "MISCELLANEOUS",
+}
+
+export interface Transaction {
+  id: string;
+  amount: number;
+  currency: string;
+  category: TransactionCategory;
+  description?: string;
+  date: string;
+  userId: string;
+}
---
+++ src/ui/components/TransactionForm/schemas.ts
@@ -0,0 +1,12 @@
+import { TransactionCategory } from "@/types/transactions";
+import { z } from "zod";
+
+export const schema = z.object({
+  amount: z.number({
+    required_error: "required field",
+    invalid_type_error: "Es necesario ingresar un monto",
+  }),
+  currency: z.string(),
+  category: z.nativeEnum(TransactionCategory),
+  description: z.string().optional(),
+});
