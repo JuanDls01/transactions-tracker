a36873faf8c7285f22f05c8755ae6edf16c58886
unknown <juanignaciodelossantos01@gmail.com>
2025-01-14 08:09:44 -0300

    feat: update transaction form to be align with new db

--- src/ui/components/TransactionForm/index.tsx
+++ src/ui/components/TransactionForm/index.tsx
@@ -1,20 +1,19 @@
 'use client';
 import { ChevronDownIcon } from '@heroicons/react/24/solid';
-import Input from '../../elements/Input';
 import Label from '../../elements/Label';
 import Select from '../../elements/Select';
 import TeaxtArea from '../../elements/TextArea';
 import { clsx } from 'clsx';
 import { useForm } from 'react-hook-form';
-import { currencyOptions, transactionCategoryOptions } from './consts';
+import { currencyOptions, transactionCategoryOptions, transactionTypeOptions } from './consts';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { schema } from './schemas';
 import { z } from 'zod';
-import { TransactionCategory } from '@/types/transactions';
 import Button from '@/ui/elements/Button';
 import { FormEvent, startTransition, useActionState, useRef } from 'react';
 import { onSubmitAction } from './actions';
 import { ActionResponse } from '@/types/actions';
+import { Currency, TransactionCategory, TransactionType } from '@prisma/client';
 
 type TransactionFormSchema = z.output<typeof schema>;
 
@@ -38,8 +37,9 @@ const TransactionForm = () => {
   } = useForm<TransactionFormSchema>({
     resolver: zodResolver(schema),
     defaultValues: {
-      currency: 'ars',
-      category: TransactionCategory.Food,
+      type: TransactionType.INCOME,
+      currency: Currency.ARS,
+      category: TransactionCategory.FOOD,
       ...(state?.inputs ?? {}),
     },
   });
@@ -65,44 +65,70 @@ const TransactionForm = () => {
       action={formAction}
       onSubmit={onSubmit}
       ref={formRef}
-      className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 space-y-2 sm:space-y-4'
+      className='bg-white shadow-md max-w-sm rounded px-8 pt-6 pb-8 mb-4 space-y-2 sm:space-y-4'
     >
       <div className='space-y-1'>
-        <Label htmlFor='amount'>Monto</Label>
-        <div
-          className={clsx(
-            'flex items-center rounded-md bg-white pl-3',
-            'outline outline-1 -outline-offset-1 outline-gray-300',
-            'has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600',
-          )}
-        >
-          <div className='shrink-0 select-none text-base text-gray-500 sm:text-sm/6'>$</div>
-          <Input
-            id='amount'
-            type='number'
-            placeholder='0.00'
-            className='text-base sm:text-sm/6 w-4/6 text-gray-900 border-none focus:outline-none focus:border-none pl-1'
-            {...register('amount', { valueAsNumber: true, required: true })}
-          />
-          <div className='grid shrink-0 grid-cols-1 focus-within:relative'>
-            <select
-              id='currency'
-              aria-label='Currency'
-              className='col-start-1 row-start-1 appearance-none rounded-md py-1.5 pl-3 pr-7 text-base text-gray-500 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6'
-              {...register('currency', { required: true })}
-            >
-              {currencyOptions.map((currency) => {
-                return (
-                  <option key={currency.key} value={currency.value}>
-                    {currency.label}
-                  </option>
-                );
-              })}
-            </select>
-            <ChevronDownIcon
-              aria-hidden='true'
-              className='pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4'
+        <Label>Movimiento</Label>
+        <div className='flex space-x-2'>
+          <select
+            id='type'
+            aria-label='Tipo de transacción'
+            {...register('type', { required: true })}
+            className={clsx(
+              'appearance-none py-2 px-4 h-max w-fit',
+              'text-base text-gray-500 sm:text-sm/6',
+              'rounded-md outline outline-1 -outline-offset-1 outline-gray-300',
+              'focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600',
+            )}
+          >
+            {transactionTypeOptions.map((currency) => {
+              return (
+                <option key={currency.key} value={currency.value}>
+                  {currency.label}
+                </option>
+              );
+            })}
+          </select>
+          <div
+            className={clsx(
+              'flex items-center rounded-md bg-white pl-3',
+              'outline outline-1 -outline-offset-1 outline-gray-300',
+              'has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600',
+            )}
+          >
+            <div className='shrink-0 select-none text-base text-gray-500 sm:text-sm/6 w-1/12'>$</div>
+            <input
+              id='amount'
+              type='number'
+              aria-label='Monto'
+              placeholder='0.00'
+              className={clsx(
+                'text-base sm:text-sm/6 text-gray-900 appearance-none w-9/12',
+                'border-none outline-none focus:outline-none focus:border-none',
+                '[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none',
+              )}
+              {...register('amount', { valueAsNumber: true, required: true })}
             />
+            <div className='grid shrink-0 grid-cols-1 focus-within:relative h-full w-3/12'>
+              <select
+                id='currency'
+                aria-label='Moneda'
+                className='col-start-1 row-start-1 appearance-none rounded-md py-1.5 pl-3 pr-4  text-base text-gray-500 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6'
+                {...register('currency', { required: true })}
+              >
+                {currencyOptions.map((currency) => {
+                  return (
+                    <option key={currency.key} value={currency.value}>
+                      {currency.label}
+                    </option>
+                  );
+                })}
+              </select>
+              <ChevronDownIcon
+                aria-hidden='true'
+                className='col-start-1 row-start-1 pointer-events-none mr-2 size-3 self-center justify-self-end text-gray-500'
+              />
+            </div>
           </div>
         </div>
         <p role='alert' className='text-red-500 text-xs italic'>
