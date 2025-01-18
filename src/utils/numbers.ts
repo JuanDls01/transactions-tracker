664d769a260320cccd247226247615d15549bdfb
unknown <juanignaciodelossantos01@gmail.com>
2025-01-18 00:33:16 -0300

    feat: added balance by currency and last 5 transactions to home page

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -2,7 +2,7 @@ import { Decimal } from '@prisma/client/runtime/library';
 
 // Transform decimal to string with dot and comma.
 // E.g.: 1000.00 => 1,000.00
-export const parseDecimalToString = (decimal: Decimal) =>
+export const parseDecimalToString = (decimal: Decimal | number) =>
   Number(decimal).toLocaleString('en-US', {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
