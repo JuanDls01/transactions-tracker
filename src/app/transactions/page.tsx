7e5372de7660bc4301e80cbcee82729ffb669e09
unknown <juanignaciodelossantos01@gmail.com>
2025-01-19 12:45:27 -0300

    refactor(transactions): use card component in transactions page

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -2,26 +2,46 @@ import prismaDb from '@/lib/prisma';
 import { transactionColumns } from '../../ui/components/transaction-columns';
 import { DataTable } from '../../ui/components/data-table';
 import { parseDecimalToString } from '@/utils/numbers';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/ui/elements/card';
+import Link from 'next/link';
 
 const MovementsPage = async () => {
   const transactions = await getTransactions();
-  if (!transactions) return <h1>No transactions found</h1>;
-  const formattedTransactions = transactions.map((t) => ({
-    ...t,
-    amount: parseDecimalToString(t.amount),
-  }));
+  if (!transactions.length) return <h1>Aún no has registrado transacciones</h1>;
   return (
-    <section className='flex flex-col space-y-4'>
-      <h2>Registro de transacciones:</h2>
-      <DataTable columns={transactionColumns} data={formattedTransactions} />
-    </section>
+    <main className='flex flex-col space-y-4'>
+      <Card className='border-none py-6 space-y-4'>
+        <CardHeader>
+          <CardTitle>Registro de transacciones</CardTitle>
+          <CardDescription>
+            Estas son todas tus transacciones transacciones. Si quieres agregar una nueva haz click{' '}
+            <Link href={'/transactions/new'} className='underline underline-offset-4'>
+              aquí
+            </Link>
+          </CardDescription>
+        </CardHeader>
+        <CardContent className='px-0 sm:px-0'>
+          <DataTable columns={transactionColumns} data={transactions} />
+        </CardContent>
+      </Card>
+    </main>
   );
 };
 
 const getTransactions = async () => {
   try {
-    const transactions = await prismaDb.transaction.findMany();
-    return transactions;
+    const user = await prismaDb.user.findFirst();
+    const transactions = await prismaDb.transaction.findMany({
+      where: { authorId: user?.id },
+      orderBy: { createdAt: 'desc' },
+    });
+
+    const formattedTransactions = transactions.map((t) => ({
+      ...t,
+      amount: parseDecimalToString(t.amount),
+    }));
+
+    return formattedTransactions;
   } catch (error) {
     console.error('Error scanning items:', error);
     throw error;
