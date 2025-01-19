65066aab4a484ef7fb0ba21d55865dde6f924164
unknown <juanignaciodelossantos01@gmail.com>
2025-01-19 18:34:40 -0300

    feat(pagination): added pretty simple pagination to transactions page

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -4,9 +4,22 @@ import { DataTable } from '../../ui/components/data-table';
 import { parseDecimalToString } from '@/utils/numbers';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/ui/elements/card';
 import Link from 'next/link';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/ui/elements/pagination';
 
-const MovementsPage = async () => {
-  const transactions = await getTransactions();
+const MovementsPage = async (props: {
+  searchParams?: Promise<{
+    page?: string;
+  }>;
+}) => {
+  const searchParams = await props.searchParams;
+  const currentPage = Number(searchParams?.page) || 1;
+  const { transactions } = await getTransactions(currentPage);
   if (!transactions.length) return <h1>Aún no has registrado transacciones</h1>;
   return (
     <main className='flex flex-col space-y-4'>
@@ -20,28 +33,49 @@ const MovementsPage = async () => {
             </Link>
           </CardDescription>
         </CardHeader>
-        <CardContent className='px-0 sm:px-0'>
+        <CardContent className='px-0 sm:px-0 space-y-4'>
           <DataTable columns={transactionColumns} data={transactions} />
+          <Pagination>
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationPrevious href={`/transactions?page=${currentPage - 1}`} />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationNext href={`/transactions?page=${currentPage + 1}`} />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
         </CardContent>
       </Card>
     </main>
   );
 };
 
-const getTransactions = async () => {
+const DEFAULT_PAGE_SIZE = 10;
+
+const getTransactions = async (currentPage: number) => {
   try {
     const user = await prismaDb.user.findFirst();
-    const transactions = await prismaDb.transaction.findMany({
-      where: { authorId: user?.id },
-      orderBy: { createdAt: 'desc' },
-    });
+    const [transactions, totalRecords] = await Promise.all([
+      prismaDb.transaction.findMany({
+        where: { authorId: user?.id },
+        orderBy: { createdAt: 'desc' },
+        skip: DEFAULT_PAGE_SIZE * (currentPage - 1),
+        take: DEFAULT_PAGE_SIZE,
+      }),
+      prismaDb.transaction.count({
+        where: {
+          authorId: user?.id,
+        },
+      }),
+    ]);
 
     const formattedTransactions = transactions.map((t) => ({
       ...t,
       amount: parseDecimalToString(t.amount),
     }));
 
-    return formattedTransactions;
+    return { transactions: formattedTransactions, totalPages: Math.ceil(totalRecords / DEFAULT_PAGE_SIZE) };
   } catch (error) {
     console.error('Error scanning items:', error);
     throw error;
