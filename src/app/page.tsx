a268873aaa5f269ffca545ee2ec04411f454935c
unknown <juanignaciodelossantos01@gmail.com>
2025-02-07 09:23:42 -0300

    fix: fixed user session validation in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import { Card, CardDescription, CardHeader, CardTitle } from '@/ui/elements/card
 import { PiggyBank } from 'lucide-react';
 import React from 'react';
 import { auth } from './auth';
+import Link from 'next/link';
 
 const Home = async () => {
   const session = await auth();
+  const isLoggedIn = session && session.user;
 
   return (
     <main className='h-72 py-10'>
@@ -15,7 +17,7 @@ const Home = async () => {
             Tu Chanchito <PiggyBank />
           </CardTitle>
           <CardDescription>Esta es la Landing Page de Tu Chanchito</CardDescription>
-          {session && <SignInButton />}
+          {isLoggedIn ? <Link href='/dashboard'>Ir al dashboard</Link> : <SignInButton />}
         </CardHeader>
       </Card>
     </main>
