e78150d63f61606e47e4a32bf8609c6be6425fc1
unknown <juanignaciodelossantos01@gmail.com>
2025-02-23 16:45:02 -0300

    feat(landing-page): restyled landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,34 @@
 import SignInButton from '@/ui/components/signin-button';
-import { Card, CardDescription, CardHeader, CardTitle } from '@/ui/elements/card';
-import { PiggyBank } from 'lucide-react';
-import React from 'react';
 import { auth } from './auth';
 import Link from 'next/link';
+import { Button } from '@/ui/elements/button';
 
 const Home = async () => {
   const session = await auth();
   const isLoggedIn = session && session.user;
 
   return (
-    <main className='h-72 py-10'>
-      <Card>
-        <CardHeader>
-          <CardTitle className='flex gap-x-3'>
-            Tu Chanchito <PiggyBank />
-          </CardTitle>
-          <CardDescription>Esta es la Landing Page de Tu Chanchito</CardDescription>
-          {isLoggedIn ? <Link href='/dashboard'>Ir al dashboard</Link> : <SignInButton />}
-        </CardHeader>
-      </Card>
+    <main className='container'>
+      <section className='w-full py-16'>
+        <div className='max-w-xl'>
+          <h1 className='mt-4 md:mt-6 text-3xl font-extrabold'>
+            Toma el control de tus finanzas de manera fácil y rápida 💸
+          </h1>
+          <p className='mt-4 md:mt-6 text-base/7 text-muted-foreground'>
+            Registra tus ingresos y gastos, visualiza tus hábitos financieros y ahorra de forma inteligente
+            con TuChanchito.
+          </p>
+          <div className='mt-4 md:mt-6'>
+            {isLoggedIn ? (
+              <Button>
+                <Link href='/dashboard'>Empecemos</Link>
+              </Button>
+            ) : (
+              <SignInButton />
+            )}
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
