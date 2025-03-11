adf6ca90a793752aa4700bea8c7a8d16ba2a0af1
unknown <juanignaciodelossantos01@gmail.com>
2025-03-11 10:10:54 -0300

    feat(auth): created withAuth callback to validate if user is authenticated

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,3 +1,4 @@
+import { auth } from '@/app/auth';
 import { Session } from 'next-auth';
 
 /**
@@ -5,4 +6,19 @@ import { Session } from 'next-auth';
  */
 export const getIfUserIsAuthenticated = (session: Session | null) => {
   return !!session && !!session.user;
+};
+
+export const getAuthenticatedUser = async () => {
+  const session = await auth();
+  if (!session?.user?.id) {
+    throw new Error('User not authenticated');
+  }
+  return session.user.id;
+};
+
+export const withAuth = <T>(fn: (userId: string) => Promise<T>) => {
+  return async () => {
+    const userId = await getAuthenticatedUser();
+    return fn(userId);
+  };
 };
